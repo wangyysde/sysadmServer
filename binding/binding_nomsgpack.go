395424c4c8ec50c0ae11cc0d43eaf94f947f78f1
wangyysde <net_use@bzhy.com>
2021-07-14 21:58:08 +0800

    bak_before_fork_gin
    Signed-off-by: wangyysde <net_use@bzhy.com>

--- binding/binding_nomsgpack.go
+++ binding/binding_nomsgpack.go
@@ -1,23 +1,6 @@
-/**
-  * SYSADM Server
-  * @Author  Wayne Wang <net_use@bzhy.com>                                                                                                                                                                                                ↷
-  * @Copyright Bzhy Network
-  * @HomePage http://www.sysadm.cn                                                                                                                                                                                                         
-  * @Version 0.1.0
-  * Licensed under the Apache License, Version 2.0 (the "License");
-  * you may not use this file except in compliance with the License.
-  * You may obtain a copy of the License at
-  * http://www.apache.org/licenses/LICENSE-2.0
-  * Unless required by applicable law or agreed to in writing, software
-  * distributed under the License is distributed on an "AS IS" BASIS,
-  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  * See the License for the specific language governing permissions and 
-  * limitations under the License.
-  * @License GNU Lesser General Public License  https://www.sysadm.cn/lgpl.html
-  * @Modified Jul 09 2021
-*/
 
 // +build nomsgpack
+
 
 package binding
 
@@ -60,7 +43,7 @@ type BindingUri interface {
 
 // StructValidator is the minimal interface which needs to be implemented in
 // order for it to be used as the validator engine for ensuring the correctness
-// of the request. Gin provides a default implementation for this using
+// of the request. sysadmServer provides a default implementation for this using
 // https://github.com/go-playground/validator/tree/v8.18.2.
 type StructValidator interface {
 	// ValidateStruct can receive any kind of type and it should never panic, even if the configuration is not right.
